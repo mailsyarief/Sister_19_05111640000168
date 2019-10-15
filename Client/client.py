687499f27be94628668ed55b39a339e4f12d150e
Mail Syarief <ismail8syarief@gmail.com>
2019-10-15 16:53:56 +0700

    tugas 2

--- Client/client.py
+++ Client/client.py
@@ -1,35 +1,38 @@
 import Pyro4
+import Pyro4.errors
+import time
+import threading
 import os
-import numpy as np
+import sys
+import uuid
+from heartbeat import Heartbeat
 
-uri = "PYRONAME:maile@localhost:7777"
+id = None
+interval = 0
+server = None
+connected = True
 
 def client_create(filename,value):
-    server = Pyro4.Proxy(uri)
     print '\n'
     print(server.create(filename,value))
     print '\n'
 
 def client_read(filename):
-    server = Pyro4.Proxy(uri)
     print '\n'
     print(server.read(filename))
     print '\n'
 
 def client_update(filename,value):
-    server = Pyro4.Proxy(uri)
     print '\n'
     print(server.update(filename,value))
     print '\n'
 
 def client_delete(filename):
-    server = Pyro4.Proxy(uri)
     print '\n'
     print(server.delete(filename))
     print '\n'
 
 def client_show():
-    server = Pyro4.Proxy(uri)
     print '\n'
     print '\n'.join(server.show())
     print '\n'
@@ -48,7 +51,129 @@ def client_send(filename):
         print "File Tidak Ada :("
 
 
+def get_server(id):
+    try:
+        uri = "PYRONAME:{}@localhost:7777".format(id)
+        gserver = Pyro4.Proxy(uri)
+        return gserver
+    except:
+        gracefully_exits()
+
+def job_heartbeat():
+    global id
+    heartbeat = Heartbeat(id)
+    t1 = threading.Thread(target=job_heartbeat_failure, args=(heartbeat,))
+    t1.start()
+
+    t = threading.Thread(target=expose_function_heartbeat, args=(heartbeat, id,))
+    t.start()
+    return heartbeat, t, t1
+
+def job_heartbeat_failure(heartbeat):
+    while True:
+        if time.time() - heartbeat.last_received > 2*interval:
+            print("\nserver is down [DETECT BY heartbeat]")
+            break
+        time.sleep(interval)
+    gracefully_exits()
+
+def expose_function_heartbeat(heartbeat, id):
+    __host = "localhost"
+    __port = 7777
+    daemon = Pyro4.Daemon(host = __host)
+    ns = Pyro4.locateNS(__host, __port)
+    uri_server = daemon.register(heartbeat)
+    ns.register("maile-{}".format(id), uri_server)
+    daemon.requestLoop()
+
+
+def job_heartbeat_failure(heartbeat):
+    while True:
+        if time.time() - heartbeat.last_received > 2*interval:
+            print("\nserver is down [DETECT BY heartbeat]")
+            break
+        time.sleep(interval)
+    gracefully_exits()
+
+
+def communicate():
+    try:
+        res = server.ok()
+        if res.value == 'ok':
+            pass
+    except:
+        return False
+    return True
+
+def ping_server():
+    global connected
+    while True and connected:
+        alive = communicate()
+        if not alive:
+            alive = communicate()
+            if not alive:
+                print("\nserver is down [DETECT BY ping ack]")
+                break
+        time.sleep(interval)
+    gracefully_exits()
+
+def job_ping_server_ping_ack():
+    t = threading.Thread(target=ping_server)
+    t.start()
+    return t
+
+def gracefully_exits():
+    # unregister device on server
+    server.connected_device_delete(id)
+    print("disconnecting..")
+    time.sleep(0.5)
+    try:
+        sys.exit(0)
+    except SystemExit:
+        os._exit(0)
+
+def clear_connected_device(devices, id):
+    if id in devices:
+        devices.remove(id)
+    return devices
+
+def all_to_al_heartbeat_job(heartbeat, devices):
+    for device in devices:
+        heartbeat.new_thread_job(device)
+
 if __name__=='__main__':
+
+    # device id
+    id = str(uuid.uuid4())
+    print('---------- registered id : {}'.format(id))
+    # core
+    server = get_server('maile')
+    try:
+        interval = server.ping_interval()
+    except:
+        print('server not running')
+        sys.exit(0)
+    server._pyroTimeout = interval
+    server._pyroAsync()
+
+
+    # register device on server (heartbeat)
+    server.connected_device_add(id)
+
+    heartbeat, thread_heartbeat, thread_heartbeat_detector = job_heartbeat()
+    thread_ping_ack = job_ping_server_ping_ack()
+
+    # register failure detector on server
+    server.new_thread_job(id)
+
+    conn_device = server.connected_device_ls()
+    conn_device.ready
+    conn_device.wait(1)
+    conn_device = clear_connected_device(conn_device.value.split(','), id)
+
+    all_to_al_heartbeat_job(heartbeat, conn_device)
+
+
     file = ""
     while True:
         print("1. Create | 2. Read | 3. Update | 4. Delete | 5. Show | 6. Send | 0. Exit")
@@ -77,3 +202,9 @@ if __name__=='__main__':
             exit()
         else:
             print("Wrong Input")
+
+    connected = False
+    thread_ping_ack.join()
+    # thread_heartbeat.join()
+    # thread_heartbeat_detector.join()
+    gracefully_exits()
--- Server/backend.py
+++ Server/backend.py
@@ -1,10 +1,92 @@
+import shlex
 import os
+import time
+import Pyro4
+import Pyro4.errors
+import json
+import threading
+
 
 class Backend(object):
 
     def __init__(self):
-        pass
+        self.connected_device = []
+        self.connected_device_thread_job = []
 
+    @Pyro4.expose
+    def connected_device_list(self):
+        return 'connected device : '+', '.join(self.connected_device)
+
+    @Pyro4.expose
+    def connected_device_ls(self):
+        return ','.join(self.connected_device)
+
+    @Pyro4.expose
+    def connected_device_add(self, id):
+        print('register '+ id)
+        self.connected_device.append(id)
+
+    @Pyro4.expose
+    def connected_device_delete(self, id):
+        print('unregister '+ id)
+        self.connected_device.remove(id)
+
+    @Pyro4.expose
+    def command_not_found(self):
+        return "command not found"
+
+    @Pyro4.expose
+    def command_success(self):
+        return "operation success"
+
+    @Pyro4.expose
+    def bye(self):
+        return "bye!"
+
+    @Pyro4.expose
+    def ok(self):
+        return "ok"
+
+    @Pyro4.expose
+    def fail(self):
+        return "fail"
+
+    @Pyro4.expose
+    def ping_interval(self):
+        return 3
+
+    @Pyro4.expose
+    def max_retries(self):
+        return 2
+
+    @Pyro4.expose
+    def new_thread_job(self, id):
+        t = threading.Thread(target=self.__new_thread_job, args=(id,))
+        t.start()
+        self.connected_device_thread_job.append(t)
+        return self.ok()
+
+    def __connect_heartbeat_server(self, id):
+        time.sleep(self.ping_interval())
+        try:
+            uri = "PYRONAME:maile-{}@localhost:7777".format(id)
+            server = Pyro4.Proxy(uri)
+        except:
+            return None
+        return server
+
+    def __new_thread_job(self, id):
+        server = self.__connect_heartbeat_server(id)
+        while True:
+            try:
+                res = server.signal_heartbeat()
+                print(res)
+            except (Pyro4.errors.ConnectionClosedError, Pyro4.errors.CommunicationError) as e:
+                print(str(e))
+                break
+            time.sleep(self.ping_interval())
+
+    @Pyro4.expose
     def create(self,filename="",value=""):
         path = os.getcwd()
         name = filename
@@ -14,6 +96,7 @@ class Backend(object):
         f.close()
         return "[ Nama : {}, Isi : {} ]".format(name,value)
 
+    @Pyro4.expose
     def update(self,filename="",value=""):
         path = os.getcwd()
         name = filename
@@ -26,6 +109,7 @@ class Backend(object):
         else:
             return "File Tidak Ada :("
 
+    @Pyro4.expose
     def read(self,filename=""):
         path = os.getcwd()
         filename = os.path.join(path, filename)
@@ -38,7 +122,7 @@ class Backend(object):
         else:
             return "File Tidak Ada :("
 
-
+    @Pyro4.expose
     def delete(self,filename=""):
         path = os.getcwd()
         filename = os.path.join(path, filename)
@@ -48,6 +132,7 @@ class Backend(object):
         else:
             return "File Tidak Ada :("
 
+    @Pyro4.expose
     def show(self):
         files = []
         path = os.getcwd()
